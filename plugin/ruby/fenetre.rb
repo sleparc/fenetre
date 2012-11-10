9bb23cdd43d75f5dc8418124836b54bd942121e4
Simon Le Parc <simon.leparc@outright.com>
2012-11-09 17:17:54 -0800

    refactored open_session (moved method)

--- plugin/ruby/fenetre.rb
+++ plugin/ruby/fenetre.rb
@@ -58,34 +58,34 @@ class Fenetre
     #
 
     def open_session
+      positions = YAML::load(File.read(SESSION_FILE))
+
       @first_window = true
       @level = -1
       @dirs = ["open_split", "open_v_split"]
-
-      def open_windows(windows, direction_action)
-        first_one = true
-        windows.each do |window|
-          if window.is_a?(Array)
-            @level += 1
-            open_windows(window, @dirs[@level % 2])
-          else
-            action = @first_window ? "first_window" : (first_one ? @dirs[(@level+1) % 2] : direction_action)
-            action = direction_action if (@level == -1)
-            first_one = false
-            @first_window = false
-            send(action, window)
-          end
-        end
-
-        @level -= 1
-      end
-
-      positions = YAML::load(File.read(SESSION_FILE))
 
       open_windows(positions, @dirs[@level % 2])
     end
 
     private
+
+    def open_windows(windows, direction_action)
+      first_one = true
+      windows.each do |window|
+        if window.is_a?(Array)
+          @level += 1
+          open_windows(window, @dirs[@level % 2])
+        else
+          action = @first_window ? "first_window" : (first_one ? @dirs[(@level+1) % 2] : direction_action)
+          action = direction_action if (@level == -1)
+          first_one = false
+          @first_window = false
+          send(action, window)
+        end
+      end
+
+      @level -= 1
+    end
 
     def open_split(window)
       VIM.command("split")
